5327ce62233c29a72b6802a9f47cc58f00397052
Balin <tn@mail.ru>
2013-09-30 16:12:37 -0400

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by XLSEsp.rc 
+//
+#define IDS_ALL_RIGHTS_RESERVED         2003
+#define IDS_PRODUCT_NAME                (IDS_XLSSTRING_RES_BASE + 1)
+#define IDS_PRODUCT_VERSION             (IDS_XLSSTRING_RES_BASE + 2)
+#define IDS_PRODUCT_NAME_AND_VERSION    (IDS_XLSSTRING_RES_BASE + 3)
+#define IDS_COPYRIGHT                   (IDS_XLSSTRING_RES_BASE + 4)
+#define IDS_COPYRIGHT_LONG              (IDS_XLSSTRING_RES_BASE + 5)
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
